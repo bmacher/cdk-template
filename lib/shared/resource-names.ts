56467ecce0f5ae3166f53f73d55668dcf9d53cd3
Benjamin Macher <macher.benjamin@icloud.com>
2021-01-17 14:34:27 +0100

    feat: add shared

---
+++ lib/shared/resource-names.ts
@@ -0,0 +1,14 @@
+/**
+ * SSM Parameter names to share informations across different stacks.
+ */
+export const ssmParamNames = {};
+
+/**
+ * Bucket names.
+ */
+export const bucketNames = {};
+
+/**
+ * Role names.
+ */
+export const roleNames = {};
